264a5b06ce6ed812a684f0f764ae7f3ad9e544bb
Gopi Goswami <grgoswami@gmail.com>
2020-11-02 22:56:25 -0500

    Added important websites

--- source/reg0.py
+++ source/reg0.py
@@ -1,4 +1,13 @@
 
+# Open source software:
+# https://en.wikipedia.org/wiki/Open-source_software
+
+# Toast masters:
+# https://www.toastmasters.org/
+
+# Python GitHub contributors website:
+# https://github.com/python/cpython/graphs/contributors
+
 # print is a function
 # 'Hello World' is a string
 # ( ) are called parentheses
