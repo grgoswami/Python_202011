c23046e16be19e603c86e7699eccefafb1fa33fa
Gopi Goswami <grgoswami@gmail.com>
2020-11-09 21:38:02 -0500

    Initial commit

---
+++ source/reg2.py
@@ -0,0 +1,66 @@
+
+# Warren Buffet
+# https://www.youtube.com/watch?v=ee5BzwBCERE
+
+# Variables are usually of three types: int, float, str
+# int are whole numbers
+# floats are numbers with decimals
+# str are strings
+
+count = 3
+print(count)
+
+result = (3 + 4.01) * 23
+print(result)
+
+text = 'How are you?'
+print(text)
+
+str0 = 'Goodbye'
+print(str0)
+
+str1 = ';askfjasfashfdahjk'
+print(str1)
+
+str1 = 'Hello!'
+print(str1)
+
+str1 = 10.234
+print(str1)
+
+name_of_school = 'Princeton'
+print(name_of_school)
+
+n_school = 'Princeton'
+print(n_school)
+
+str2 = "Rutgers"
+print(str2)
+
+int1 = -100
+print(int1)
+
+float1 = 2.34
+print(float1)
+
+# Name of a variable using Camel casing
+# myList or my_list or list0 or l0
+# The following is a list. Lists can have int, float, str as its 'elements'
+# [] are called square braces
+myList = [1, 2, 3, 4.5, 9.7, 'Aha', 'Awesome', 100]
+len(myList)
+
+length = 3
+width = 4
+area = width * length
+print(area)
+
+# (length + width) is for grouping mathematical expressions
+perimeter = 2 * (length + width)
+# (periemeter) is for calling a function or function call parentheses
+print(perimeter)
+
+
+
+
+
