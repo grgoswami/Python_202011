99fc59a59d0c943a29ba1c8796b4c35d8060ba9d
Gopi Goswami <grgoswami@gmail.com>
2020-11-09 21:41:12 -0500

    Initial commit

---
+++ source/Anya0.py
@@ -0,0 +1,49 @@
+#We are going to learn about varibals.
+#These are examples of varibals.
+
+#Let me show you some first.
+
+#What is a varibal?
+#A varibal is something that holds information that you wnat to use later.
+#Varibals can be of different types such as intergers(ints), flots, and/or strings.
+
+#Whole numbers are called ints or intergers.
+#1
+#143
+
+#Decimals are called flots.
+#It is not a hole numbers.
+#9.234
+#3.212
+
+#Anything in quots are called strings.
+#"Sjf;aos f"
+#"how are you "
+
+print("5+6")
+
+asj_dfhasif = (1+1)
+print(asj_dfhasif)
+
+b = (3)
+print(b)
+
+c = ("asdfhalsfdu")
+print(c)
+
+d = ("Hi!")
+print(d)
+
+e = (2.000)
+print(e)
+
+f = 20.001
+print(f)
+
+g = "How are you?"
+print(g)
+
+g = "I am good! :)"
+print(g)
+
+print(g)
