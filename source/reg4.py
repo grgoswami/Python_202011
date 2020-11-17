c95268984d486e5c07c2ee89f7998c61f70bb484
Gopi Goswami <grgoswami@gmail.com>
2020-11-16 21:35:23 -0500

    Initial commit

---
+++ source/reg4.py
@@ -0,0 +1,31 @@
+
+s0 = '12'
+print(type(s0))
+print(s0)
+
+i0 = int(s0)
+print(type(i0))
+print(i0)
+
+s1 = "11.234"
+print(type(s1))
+print(s1)
+
+f1 = float(s1)
+print(type(f1))
+print(f1)
+
+s2 = 'Trump'
+print(s2)
+
+s3 = '-100'
+i3 = int(s3)
+
+print(type(s3))
+print(type(i3))
+
+s4 = '3.14'
+f4 = float(s4)
+
+print(type(s4))
+print(type(f4))
---
+++ source/reg5.py
@@ -0,0 +1,14 @@
+cars = ['Toyota', 'Honda', 'Mazda']
+print(type(cars))
+
+# Indexing
+# Indexing starts with 0 in Python
+print(cars[1])
+print(cars[-1])
+print(cars[-2])
+print(cars[-3])
+print(cars[-4])
+print(cars[10])
+
+# Slicing
+print(cars[1:3])
