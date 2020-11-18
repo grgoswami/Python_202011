32f1cd8c1cbd2e5dae61ad285b4fb68398385e93
Gopi Goswami <grgoswami@gmail.com>
2020-11-17 21:36:57 -0500

    Initial commit

---
+++ source/Tista0.py
@@ -0,0 +1,10 @@
+fruits = ['apple', 'banana', 'orange', 'grapes', 'watermelon']
+
+print(fruits)
+print(fruits[1])
+print(fruits[-1])
+print(fruits[1:3])
+print(fruits[1:4])
+print(fruits[:])
+print(fruits[2:])
+print(fruits[:4])
