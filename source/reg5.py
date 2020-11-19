9e1792e8ca4acae81cee3b966b3495892f7fa0c5
Gopi Goswami <grgoswami@gmail.com>
2020-11-18 21:41:02 -0500

    Added more examples

--- source/reg5.py
+++ source/reg5.py
@@ -1,14 +1,55 @@
 cars = ['Toyota', 'Honda', 'Mazda']
 print(type(cars))
+print(cars)
 
 # Indexing
-# Indexing starts with 0 in Python
+# Indexing starts with 0 in Python at the start of the list
+# Indexing starts with -1 in Python at the end of the list
 print(cars[1])
 print(cars[-1])
 print(cars[-2])
 print(cars[-3])
 print(cars[-4])
 print(cars[10])
+print(cars[2])
 
-# Slicing
+ice_creams = ['vanilla', 'chocolate', 'butter pecan', 'rocky road']
+print(ice_creams[-2])
+print(ice_creams[2])
+
+l0 = [3, 4]
+print(l0)
+
+l1 = ['3', '4']
+print(l1)
+
+l2 = [3, '4']
+print(l2)
+
+l3 = [3, '4', 5.91]
+print(l3)
+
+l4 = [3, -10, -5.34, '4', 5.91, 'New York']
+print(l4)
+
+# Slicing 
+# Slicing looks like start:end or start: or :end or :
+# The end is never touched in Python 
+# start you touch, end you don't
+print(cars)
 print(cars[1:3])
+print(cars[1:])
+print(cars[:2])
+# The following is called a copy of a list
+print(cars[:])
+
+ice_creams = ['vanilla', 'chocolate', 'butter pecan', 'rocky road']
+print(ice_creams[:-2])
+print(ice_creams[-2:])
+print(ice_creams[-4:-1])
+
+
+
+
+
+
