6f88ef057d0ef23a0d0efa6509cb9ff1a49f62e8
Gopi Goswami <grgoswami@gmail.com>
2020-11-23 21:38:33 -0500

    Added more examples

--- source/reg5.py
+++ source/reg5.py
@@ -47,6 +47,16 @@ ice_creams = ['vanilla', 'chocolate', 'butter pecan', 'rocky road']
 print(ice_creams[:-2])
 print(ice_creams[-2:])
 print(ice_creams[-4:-1])
+print(ice_creams)
+print(ice_creams[-3:-2])
+
+# list assignment
+ice_creams[1] = 'strawberry'
+print(ice_creams)
+
+ice_creams[-2] = 'peach'
+print(ice_creams)
+
 
 
 
