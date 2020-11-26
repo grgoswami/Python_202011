feff009a22fc87351733148c658eb9e48725bc23
Gopi Goswami <grgoswami@gmail.com>
2020-11-25 21:39:02 -0500

    Initial commit

---
+++ source/reg6.py
@@ -0,0 +1,16 @@
+# String indexing
+
+str0 = 'Tista loves chocolate'
+print(len(str0))
+
+print(str0[3])
+
+# String slicing
+print(str0[5:7])
+
+print(str0[4:7])
+
+# String mutation
+# Strings are not 'mutable'; they are called immutable
+str0[3] = 'z'
+print(str0)
