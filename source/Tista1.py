25c458d885027b9ae2593122354f1bc6b7fc30c6
Gopi Goswami <grgoswami@gmail.com>
2020-12-01 11:57:27 -0500

    Initial commit

---
+++ source/Tista1.py
@@ -0,0 +1,28 @@
+s = 'Hello'
+print(s)
+#slicing
+print(s[:8])
+print(s[:])
+print(s[3:])
+#indexing
+print(s[-2])
+print(s[-3])
+print(s[2])
+print(s[10])
+
+print(len(s))
+
+q = 'qwert'
+print(len(q))
+
+sentence = 'I like ice cream'
+
+sen = sentence.split()
+print(sen)
+print(len(sen))
+
+print(len('Hi, my name is Tista'))
+
+name = 'Tista'
+
+print('Your name has ' + str(len(name)) + ' letters')
