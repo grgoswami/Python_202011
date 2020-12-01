3b1c9980d47d2785eb07fb086b3aba49198bcd61
Gopi Goswami <grgoswami@gmail.com>
2020-11-30 22:40:25 -0500

    Initial commit

---
+++ source/Anya1.py
@@ -0,0 +1,34 @@
+#We are going to learn how to convert ints and floats into strings.
+#Thees and examples of ints and floats into strings.
+
+#First ints into strings.
+
+a = 1
+b = str(a)
+print(b)
+
+print("b is a string " + b)
+
+print("b is a string " + a)
+
+#Now that b holds the value of a as a string, you can not use it in a calcuation.
+
+z = 3+b
+print(z)
+
+x = 3+a
+print(x)
+
+#Now let's do floats into strings.
+
+e = 1.2
+f = str(e)
+print(f)
+
+#Let's make sure that it is a string.
+
+i = 4+f
+print(i)
+
+j = 5+e
+print(j)
