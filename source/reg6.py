e9fb543a2eb5e928f552ea4db7fcb6e491aaa2f5
Gopi Goswami <grgoswami@gmail.com>
2020-11-30 21:39:34 -0500

    Added more examples

--- source/reg6.py
+++ source/reg6.py
@@ -14,3 +14,15 @@ print(str0[4:7])
 # Strings are not 'mutable'; they are called immutable
 str0[3] = 'z'
 print(str0)
+
+s2 = 'New York'
+zip_code = 10001
+
+# The following is called string concatenation
+print(s2 + zip_code)
+print(s2 + str(zip_code))
+print(s2 + ' ' + str(zip_code))
+
+s3 = 'New York '
+print(s3 + str(zip_code))
+ 
