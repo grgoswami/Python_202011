3ac78b0d3b54c198ec864b285b3b8bb19c00a1fa
Gopi Goswami <grgoswami@gmail.com>
2020-12-16 21:35:25 -0500

    Initial commit

---
+++ source/reg11.py
@@ -0,0 +1,14 @@
+
+t0 = ('New York', 10001, 45)
+print(t0)
+
+# Immutability of tuples
+t0[0] = 1
+
+# The right side of = is called tuple packing
+# The left side of = is called tuple unpacking
+v0, v1, v2 = 'New York', 10001, 45
+print(v0)
+print(v1)
+print(v2)
+
