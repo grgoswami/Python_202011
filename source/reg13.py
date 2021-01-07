8c3c7142e5e094afb0623410399a2bb03f4b8bec
Gopi Goswami <grgoswami@gmail.com>
2021-01-06 21:08:32 -0500

    Initial commit

---
+++ source/reg13.py
@@ -0,0 +1,25 @@
+# Some useful functions on lists
+
+l0 = []
+print(l0)
+for ci in range(5):
+    l0.append(ci + 10)
+    
+print(l0)
+    
+l1 = []
+print(l1)
+for ci in range(5):
+    l1.append(2 * ci)
+    
+print(l1)
+
+l2 = []
+print(l2)
+for ci in range(1, 6):
+    l2.append(2 * ci - 1)
+    
+print(l2)
+
+    
+    
