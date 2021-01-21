79c1f1f1900cac320bbb521afeb67f5d2acc5ef0
Gopi Goswami <grgoswami@gmail.com>
2021-01-20 21:36:27 -0500

    Initial commit

---
+++ source/reg17.py
@@ -0,0 +1,27 @@
+
+def Fibonacci0(num):
+    """
+    Parameters
+    ----------
+    num : int
+        The number of elements from the Fibonacci sequence.
+
+    Returns
+    -------
+    None. It prints the numbers.
+
+    """
+    a = 1
+    print(a)
+    b = 1
+    print(b)
+    for i in range(2, num):
+        c = a + b
+        print(c)
+        a = b
+        b = c
+
+Fibonacci0(3)
+Fibonacci0(5)
+Fibonacci0(10)
+        
